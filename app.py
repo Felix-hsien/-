a0dcf361dcff0a6918027e87456b5a9d3a808a34
Felix-hsien <superfelix2.hsien@gmail.com>
2026-05-03 10:08:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -628,7 +628,7 @@ uploaded_file = st.file_uploader("Upload", type="csv")
 if uploaded_file is not None:
     # 讀取資料
     df = pd.read_csv(uploaded_file)
-    st.table("### 上傳的資料")
+    st.markdown("### 上傳的資料")
     st.table(df)
 
     # 確保欄位名稱正確
